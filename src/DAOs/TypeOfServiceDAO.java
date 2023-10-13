70ea874596b2425c2a713e1845aa9bed756abfe9
BIGBOSS\PC <duy9367@gmail.com>
2023-10-13 21:07:01 +0700

    update TypeOfServiceDAO.java

--- src/DAOs/TypeOfServiceDAO.java
+++ src/DAOs/TypeOfServiceDAO.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import java.util.List;
 import ConnectDB.ConnectDB;
 import Entity.Room;
+import Entity.Service;
 import Entity.TypeOfRoom;
 import Entity.TypeOfService;
 
@@ -51,4 +52,94 @@ public class TypeOfServiceDAO {
         return tr;
     }
 
+    public ArrayList<TypeOfService> getTypeOfServiceByName(String name) {
+        ArrayList<TypeOfService> lst = new ArrayList<TypeOfService>();
+        ConnectDB.getInstance();
+        Connection con = ConnectDB.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            String sql = "SELECT * FROM LoaiDichVu where tenoaiDichVu like ?";
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, "%" + name + "%");
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String maLDV = rs.getString("maLoaiDichVu");
+                String tenLDV = rs.getString("tenoaiDichVu");
+                TypeOfService typeOfService = new TypeOfService(maLDV, tenLDV);
+                lst.add(typeOfService);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return lst;
+    }
+
+    public boolean insert(TypeOfService s){
+        ConnectDB.getInstance();
+        Connection con = new ConnectDB().getConnection();
+        PreparedStatement statement = null;
+        int n=0;
+        try{
+            String sql = "insert into dbo.LoaiDichVu (maLoaiDichVu,tenoaiDichVu)"+"values (?,?)";
+            statement = con.prepareStatement(sql);
+            statement.setString(1, s.getMaLoaiDichVu());
+            statement.setString(2, s.getTenLoaiDichVu());
+            n = statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return n>0;
+    }
+    public boolean update(TypeOfService s) {
+        int n = 0;
+        PreparedStatement stmt = null;
+        ConnectDB.getInstance();
+        Connection con = ConnectDB.getConnection();
+        String sql = "update dbo.LoaiDichVu set tenoaiDichVu = ?" + " Where maLoaiDichVu = ?";
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, s.getTenLoaiDichVu());
+            stmt.setString(2, s.getMaLoaiDichVu());
+            n = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return n > 0;
+    }
+    public boolean delete(String id) {
+        int n = 0;
+        PreparedStatement stmt = null;
+        ConnectDB.getInstance();
+        Connection con = ConnectDB.getConnection();
+        String sql = "delete from dbo.LoaiDichVu where maLoaiDichVu = ?";
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, id);
+
+            n = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return n > 0;
+    }
 }
