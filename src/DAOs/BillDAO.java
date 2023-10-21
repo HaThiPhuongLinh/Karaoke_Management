6d976ecb7790559e760e55ce6557e5b0786aa9b8
BIGBOSS\PC <duy9367@gmail.com>
2023-10-22 01:20:28 +0700

    Update BillDAO.java

--- src/DAOs/BillDAO.java
+++ src/DAOs/BillDAO.java
@@ -1,10 +1,7 @@
 package DAOs;
 
 import ConnectDB.ConnectDB;
-import Entity.Bill;
-import Entity.Customer;
-import Entity.Room;
-import Entity.Staff;
+import Entity.*;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -39,6 +36,43 @@ public class BillDAO {
                 String khuyenMai = rs.getString(9);
 
                 Bill bill = new Bill(maHoaDon,maNhanVien,maKhachHang,maPhong,giaPhong,ngayGioDat,ngayGioTra,tinhTrang,khuyenMai);
+
+                dsStaff.add(bill);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dsStaff;
+    }
+
+    public ArrayList<Bill> getListBillByDate(Date tuNgay, Date denNgay){
+        ArrayList<Bill> dsStaff = new ArrayList<Bill>();
+        ConnectDB.getInstance();
+        PreparedStatement statement = null;
+        Connection con = ConnectDB.getConnection();
+        try {
+            String sql = "SELECT * FROM dbo.HoaDon WHERE ngayGioTra >= ? AND ngayGioTra <= ?";
+//            String sql = "SELECT ct.* FROM ChiTietDichVu ct JOIN HoaDon hd ON ct.maHoaDon = hd.maHoaDon WHERE hd.ngayGioTra >= ? AND hd.ngayGioTra <= ?";
+            statement = con.prepareStatement(sql);
+
+            statement.setDate(1, (java.sql.Date) tuNgay);
+            statement.setDate(2, (java.sql.Date) denNgay);
+
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+
+                String maHoaDon = rs.getString(1);
+                Staff maNhanVien = new Staff(rs.getString(2));
+                Customer maKhachHang = new Customer(rs.getString(3));
+                Room maPhong = new Room(rs.getString(4));
+//                double giaPhong = rs.getDouble(5);
+
+                Timestamp ngayGioDat = rs.getTimestamp(5);
+                Timestamp ngayGioTra = rs.getTimestamp(6);
+                int tinhTrang = rs.getInt(7);
+                String khuyenMai = rs.getString(8);
+
+                Bill bill = new Bill(maHoaDon,maNhanVien,maKhachHang,maPhong,ngayGioDat,ngayGioTra,tinhTrang,khuyenMai);
 
                 dsStaff.add(bill);
             }
